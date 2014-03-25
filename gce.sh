daad750733b9babe1bf46491c3c09c9c184bbaa3
Beau Lyddon <beau.lyddon@webfilings.com>
2014-03-25 13:17:09 -0500

    Split up by docker and non docker

---
+++ gce.sh
@@ -0,0 +1,4 @@
+curl get.docker.io | bash
+sudo update-rc.d docker defaults
+
+apt-get -y install vim git make
--- graphite_stand_alone/config.js
+++ graphite_stand_alone/config.js
@@ -1,4 +1,6 @@
 {
+  //graphiteHost: process.env.HOSTNAME,
+  //graphitePort: parseInt(process.env.GRAPH_PORT_2003_TCP_PORT),
   graphiteHost: "127.0.0.1",
   graphitePort: 2003,
   port: 8125,
