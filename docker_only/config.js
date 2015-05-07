e1a75f858e2882efb1af1454f724ee0f6dcdafe1
Amandeep Gill <amandeep.gill@workiva.com>
2015-05-07 12:56:45 -0600

    put Dockerfile for lighter statsd in own folder

--- docker_only/config.js
+++ docker_only/config.js
@@ -7,16 +7,5 @@
   dumpMessages: false,
   backends: ["./backends/graphite"],
   deleteIdleStats: true,
-  percentThreshold: [90, 95, 99],
-  metricToggles: {
-    "std": true,
-    "upper": true,
-    "lower": true,
-    "count": true,          // counts per flushInterval
-    "count_ps": true,       // counts per second
-    "sum": false,           // sums of timer values for each percentThreshold
-    "sum_squares": false,   // sums of squares of timer values for each percentThreshold
-    "mean": true,
-    "median": true,
-  }
+  percentThreshold: [90, 95, 99]
 }
